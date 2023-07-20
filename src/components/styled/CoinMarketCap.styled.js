277f8d43d53734af978c0c12f4da718414bc6a4b
chelsea roberts <chelsea.j.roberts@hotmail.com>
2023-07-20 07:02:35 +0100

    market cap stlying begun

--- src/components/styled/CoinMarketCap.styled.js
+++ src/components/styled/CoinMarketCap.styled.js
@@ -10,6 +10,7 @@ export const CoinMarketCapStlye = styled.div`
     border-radius: 10px;
     margin-top: 2em;
     margin-left: 2em;
+    font-size: .85em;
 
     li{
         list-style: none;
@@ -17,7 +18,7 @@ export const CoinMarketCapStlye = styled.div`
     }
 
     .sqaure{
-        color: blue;
+        color: #3C72E6;
         font-size: 1.2em;
         margin-right: .3em;
     }
@@ -26,5 +27,27 @@ export const CoinMarketCapStlye = styled.div`
         display:flex;
         flex-direction: column;
         margin-right:0;
+    }
+
+    .red{
+        color:red
+    }
+
+    .green{
+        color:  rgb(0, 252, 42);
+        margin-left: .5em;
+    }
+
+    .tot-vol{
+        color: rgb(26, 215, 97);
+    }
+
+    .max-sup{
+        color: rgb(33, 114, 229);
+    }
+
+    .title{
+        font-weight: bold;
+        margin-right: .5em;
     }
     `
